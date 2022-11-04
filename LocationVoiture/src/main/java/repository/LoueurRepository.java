fe9a1d8173a8811243744c09c34b4038d5b7f23f
Tapotix <gaulmatthieu@gmail.com>
2022-11-04 15:58:19 +0100

    restcontroller loueur and annonce

--- LocationVoiture/src/main/java/repository/LoueurRepository.java
+++ LocationVoiture/src/main/java/repository/LoueurRepository.java
@@ -4,6 +4,7 @@ import java.util.List;
 import java.util.Optional;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
 
 import model.Annonce;
@@ -14,6 +15,9 @@ public interface LoueurRepository extends JpaRepository<Loueur, Integer>{
 	
 	
 	List<Loueur> findByAnnonceContaining(Annonce annonce);
+	
+	@Query("select l from Loueur l left join fetch l.annonce where l.id=:id")
+	Optional<Loueur> findByIdFetchingAnnonce(@Param("id") Integer id);
 
 	
 	
