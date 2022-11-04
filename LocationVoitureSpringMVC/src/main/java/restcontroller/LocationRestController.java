0be45d801684daa412e0c24616cb545385a7b1a4
Anastasia <rotari_anastasia@yahoo.com>
2022-11-04 15:20:30 +0100

    package restcontroller

---
+++ LocationVoitureSpringMVC/src/main/java/restcontroller/LocationRestController.java
@@ -0,0 +1,14 @@
+package restcontroller;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api/produit")
+public class LocationRestController {
+
+	
+	
+	
+	
+}
