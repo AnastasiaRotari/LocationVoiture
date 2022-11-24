baba86ff9fe7cbd0daa0f4e769291f95f5833663
Mohamed Kante <kantmoha@yahoo.com>
2022-11-24 15:53:05 +0100

    recap location sans validation ><

--- LocationVoitureAngular/src/app/location/component/location/location-confirm/location-confirm/location-confirm.component.ts
+++ LocationVoitureAngular/src/app/location/component/location/location-confirm/location-confirm/location-confirm.component.ts
@@ -1,14 +1,12 @@
 import { Component, OnInit } from '@angular/core';
-import { ActivatedRoute } from '@angular/router';
-import { Annonce } from 'src/app/location/model/annonce';
-import { AnnonceService } from 'src/app/location/service/annonce.service';
-
+import { Location } from 'src/app/location/model/location';
 @Component({
   selector: 'app-location-confirm',
   templateUrl: './location-confirm.component.html',
   styleUrls: ['./location-confirm.component.css'],
 })
 export class LocationConfirmComponent implements OnInit {
+  locations: Location[] = [];
   constructor() {}
 
   ngOnInit(): void {}
